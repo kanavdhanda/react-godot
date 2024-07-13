3ebefc33c3570e2a4f3d7609341b3154038b110e
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-07-14 05:17:25 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+hello world
+
+this here is to combine godot game with react web page
---
+++ core/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>React game thing testing</h1>
+    </>
+  )
+}
+
+export default App
