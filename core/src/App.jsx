acbd0291a74b4f35de9e699ecd531d2fe0c13a4c
Kanav Dhanda <70010606+kanavdhanda@users.noreply.github.com>
2024-07-15 03:55:03 +0530

    successfully fixed issue still things to sort out like compression and reusability

--- core/src/App.jsx
+++ core/src/App.jsx
@@ -1,12 +1,22 @@
-
+import {useState } from "react"
 import Godot from "./godot"
 function App() {
-
+  // const [state, setState] = useState(false)
+  // const buttonClick = () => {
+  //     setState(true)
+  // }
 
   return (
     <>
-      <h1>React game thing testing</h1>
-      <Godot className="h-[500px] w-[500px]"/>
+    <div className="relative">
+    <h1 className="absolte top-0 left-0">React game thing testing</h1>
+    {/* <button onClick={buttonClick}>Click me to play the game</button> */}
+    <Godot className="h-[500px] w-[500px]"/>
+
+{/* {state && (
+)} */}
+    </div>
+      
     </>
   )
 }
